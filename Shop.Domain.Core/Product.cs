cbedc44a30d68fa81959dc0cc7c8638bd86a3bc6
par.jd <ignatyk_a.s_by@mail.ru>
2020-02-16 11:03:14 +0300

    Create repository

---
+++ Shop.Domain.Core/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Domain.Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Shop.Domain.Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Domain.Core;
+using System;
+using System.Collections.Generic;
+
+namespace Shop.Domain.Interfaces
+{
+    public interface IProductRepository : IDisposable
+    {
+        IEnumerable<Product> GetProductList();
+        Product GetProduct(int id);
+        void AddProduct(Product product);
+        void EditProduct(Product product);
+        void DelProdect(Product product);
+        void Save();
+    }
+}
