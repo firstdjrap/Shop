cd5c19a9ecc4e2e22ee23912b9b94d0d6729deb5
Yauheni Lakhmitski <lakhmitski@outlook.com>
2020-02-22 16:33:48 +0300

    add query

--- Shop.Infrastructure.Data/PromotionRepository.cs
+++ Shop.Infrastructure.Data/PromotionRepository.cs
@@ -2,6 +2,7 @@
 using Shop.Domain.Core;
 using Shop.Domain.Interfaces;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Shop.Infrastructure.Data
@@ -9,15 +10,18 @@ namespace Shop.Infrastructure.Data
     public class PromotionRepository : IPromotionRepository
     {
         private readonly ShopContext _shopContext;
+        private readonly IQueryable<Promotion> _query;
 
         public PromotionRepository(DbContextOptions<ShopContext> connection)
         {
             _shopContext = new ShopContext(connection);
+            _query = _shopContext.Promotions
+                .Include(s => s.Products);
         }
         
         public async Task<Promotion> GetAsync(int id)
         {
-            return await _shopContext.Promotions.SingleOrDefaultAsync(x => x.Id == id);
+            return await _query.SingleOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<Promotion> AddAsync(Promotion promotion)
@@ -38,7 +42,7 @@ namespace Shop.Infrastructure.Data
 
         public async Task<IEnumerable<Promotion>> GetAsync()
         {
-            return await _shopContext.Promotions.ToListAsync();
+            return await _query.ToListAsync();
         }
     }
 }
